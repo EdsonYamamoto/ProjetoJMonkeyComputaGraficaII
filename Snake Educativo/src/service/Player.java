0e27b917aa9181b7abeb56bb10951f2f7bcba7b8
Edson Yamamoto <edsonkazyamamoto@gmail.com>
2019-05-04 15:14:34 -0300

    Outras classes

---
+++ Snake Educativo/src/service/Player.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package service;
+
+/**
+ *
+ * @author Edson
+ */
+public class Player {
+    static public void Selecionar(){
+        System.out.println("Selecionado");
+    }
+}
---
+++ Snake Educativo/src/service/Snake.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package service;
+
+/**
+ *
+ * @author Edson
+ */
+public class Snake {
+    static public void ComerLetra(){
+        System.out.println("Comer letra!");
+    }
+    static public void Colidir(){
+        System.out.println("Colidir");
+    }
+}
